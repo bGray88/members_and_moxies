02ade01afc04aa22a6b80eb8bd8869c9bec62799
Brandon Gray <brandonjoe88@hotmail.com>
2023-05-11 11:26:16 -0600

    feat: adds slugified user info to details endpoint

--- src/members/urls.py
+++ src/members/urls.py
@@ -5,6 +5,6 @@ urlpatterns = [
     path('', views.main, name='main'),
     path('members_table/', views.members_table, name='members_table'),
     path('members/', views.members, name='members'),
-    path('members/details/<int:id>', views.details, name='details'),
+    path('members/details/<slug:slug>', views.details, name='details'),
     path('testing/', views.testing, name='testing'),
 ]
